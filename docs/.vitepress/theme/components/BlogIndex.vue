04977c1c11c0122f3e5369166ca304eddd6a27a8
Bugo <cheetah@bk.ru>
2024-11-27 14:28:18 +0500

    Fix the issue with blog links

--- docs/.vitepress/theme/components/BlogIndex.vue
+++ docs/.vitepress/theme/components/BlogIndex.vue
@@ -14,7 +14,7 @@ function getDateTime(time: number) {
           post.date.string
         }}</time>
         <h2 class="title">
-          <a :href="post.url">{{ post.title }}</a>
+          <a :href="'/vite-docs' + post.url">{{ post.title }}</a>
         </h2>
       </article>
     </li>
