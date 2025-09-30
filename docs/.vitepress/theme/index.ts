db98f80a3c429e56482275d1d190d1b4e0c972ed
Bugo <cheetah@bk.ru>
2025-09-30 07:00:06 +0500

    Add a simple banner

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -8,6 +8,7 @@ import './styles/landing.css'
 import AsideSponsors from './components/AsideSponsors.vue'
 import SvgImage from './components/SvgImage.vue'
 import YouTubeVideo from './components/YouTubeVideo.vue'
+import SponsorBanner from './components/SponsorBanner.vue'
 import NonInheritBadge from './components/NonInheritBadge.vue'
 import 'virtual:group-icons.css'
 
@@ -15,6 +16,7 @@ export default {
   extends: DefaultTheme,
   Layout() {
     return h(DefaultTheme.Layout, null, {
+      'layout-top': () => h(SponsorBanner),
       'aside-ads-before': () => h(AsideSponsors),
     })
   },
