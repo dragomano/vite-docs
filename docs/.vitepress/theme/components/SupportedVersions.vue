94f9e479995640b851b6c8c87135d7cc37fc1528
Bugo <229402+dragomano@users.noreply.github.com>
2026-08-04 08:03:59 +0500

    Extract VersionsList component
    Closes #437

--- docs/.vitepress/theme/components/SupportedVersions.vue
+++ docs/.vitepress/theme/components/SupportedVersions.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { ref, computed } from 'vue'
+import VersionsList from './VersionsList.vue'
 
 declare const __VITE_VERSION__: string
 
@@ -86,15 +87,6 @@ function computeSupportInfo(
   }
 }
 
-function versionsToText(versions: string[]) {
-  versions = versions.map((v) => `<code>vite@${v}</code>`)
-  if (versions.length === 0) return ''
-  if (versions.length === 1) return versions[0]
-  return (
-    versions.slice(0, -1).join(', ') + ' и ' + versions[versions.length - 1]
-  )
-}
-
 function isValidViteVersion(version: string) {
   if (version.length === 1) version += '.'
   // Vite 0.x shouldn't be mentioned, and Vite 1.x was never released
@@ -108,15 +100,15 @@ function isValidViteVersion(version: string) {
     <ul>
       <li v-if="supportInfo.regularPatches.length">
         Регулярно выпускаются исправления для
-        <span v-html="versionsToText(supportInfo.regularPatches)"></span>.
+        <VersionsList :versions="supportInfo.regularPatches" />.
       </li>
       <li v-if="supportInfo.importantFixes.length">
         Важные исправления и исправления безопасности перенесены в
-        <span v-html="versionsToText(supportInfo.importantFixes)"></span>.
+        <VersionsList :versions="supportInfo.importantFixes" />.
       </li>
       <li v-if="supportInfo.securityPatches.length">
         Исправления безопасности также перенесены в
-        <span v-html="versionsToText(supportInfo.securityPatches)"></span>.
+        <VersionsList :versions="supportInfo.securityPatches" />.
       </li>
       <li>
         Эти версии устарели и больше не получают обновлений. Рекомендуется перейти на одну из актуальных версий.
